16acafa62afc25f9b4d149b0cc1ec2894b2bf8cb
alsocoder <spoltaracki@gmail.com>
2017-12-03 00:30:47 +0200

    point 3

--- 3dz/script.js
+++ 3dz/script.js
@@ -1,1 +1,16 @@
 alert('Im - JavaScript!');
+
+/*
+Какой скрипт выполнится первым в примере ниже :
+a)
+<script src="big.js"></script>    Run first
+<script src="small.js"></script>
+б)
+<script async src="big.js"></script>
+<script async src="small.js"></script>  Run first
+в)
+<script defer src="big.js"></script>    Run first
+<script defer src="small.js"></script>
+
+
+*/
